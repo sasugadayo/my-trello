d104c6b1f5923ea045bd0090f57e85f7b2f6cfd1
sasugadayo <sasugaff@icloud.com>
2022-01-01 01:14:10 +0900

    append

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -1,13 +1,15 @@
 <template>
   <!-- ★ここから追記 -->
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classList" @submit.prevent="addList">
     <input
       v-model="title"
       type="text"
       class="text-input"
       placeholder="Add new list"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     />
-    <button type="submit" class="add-button">
+    <button type="submit" class="add-button" v-if="isEditing || titleExists">
       Add
     </button>
   </form>
@@ -16,16 +18,38 @@
 
 <script>
 export default {
-  data: function() {
+  data: function () {
     return {
       title: "",
+      isEditing: false, // ★追記
     };
+  },
+  computed: {
+    classList() {
+      const classList = ["addlist"];
+      if (this.isEditing) {
+        classList.push("active");
+      }
+      if (this.titleExists) {
+        classList.push("addable");
+      }
+      return classList;
+    },
+    titleExists() {
+      return this.title.length > 0;
+    },
   },
   // ★ここから追記
   methods: {
-    addList: function() {
+    addList: function () {
       this.$store.dispatch("addlist", { title: this.title });
       this.title = "";
+    },
+    startEditing() {
+      this.isEditing = true;
+    },
+    finishEditing() {
+      this.isEditing = false;
     },
   },
   // ★ここまで追記
