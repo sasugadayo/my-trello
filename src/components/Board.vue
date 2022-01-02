f2d49d60f8f97113411329ff1663403bb1b34602
sasugadayo <sasugaff@icloud.com>
2022-01-02 17:41:32 +0900

    append

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,25 +1,54 @@
 <template>
   <div>
-    <header>
-      my Trello
-    </header>
+    <header>my Trello</header>
     <main>
-      <p class="info-line">All: 0 tasks</p>
+      <p class="info-line">All: {{ totalCardCount }} tasks</p>
+      <div class="list-index">
+        <draggable :list="lists" @end="movingList" class="list-index">
+          <list
+            v-for="(item, index) in lists"
+            :key="item.id"
+            :title="item.title"
+            :cards="item.cards"
+            :listIndex="index"
+            @change="movingCard"
+          />
+          <list-add />
+        </draggable>
+      </div>
       <!-- ★ここに追記 -->
-      <list-add />
     </main>
   </div>
 </template>
 
 <script>
 // ★ここに追記
+import draggable from "vuedraggable";
 import ListAdd from "./ListAdd.vue";
+import List from "./List";
+import { mapState } from "vuex";
 
 export default {
   // ★ここから追記
   components: {
+    draggable,
     ListAdd,
+    List,
   },
   // ★ここまで追記
+  computed: {
+    ...mapState(["lists"]),
+    totalCardCount() {
+      return this.$store.getters.totalCardCount;
+    },
+  },
+  methods: {
+    movingCard: function () {
+      this.$store.dispatch("updateList", { lists: this.lists });
+    },
+    movingList: function () {
+      this.$store.dispatch("updateList", { lists: this.lists });
+    },
+  },
 };
 </script>
